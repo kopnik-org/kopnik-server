9d4ff2ea61907eb525112849dd5748a6deed3827
alexey2baranov <alexey2baranov@gmail.com>
2020-01-11 13:10:14 +0300

    add unique const from identifier

--- src/Entity/UserOauth.php
+++ src/Entity/UserOauth.php
@@ -16,6 +16,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *          @ORM\Index(columns={"email"}),
  *      },
  *      uniqueConstraints={
+ *          @ORM\UniqueConstraint(columns={"identifier", "provider"}),
  *          @ORM\UniqueConstraint(columns={"email", "provider"}),
  *          @ORM\UniqueConstraint(columns={"access_token"}),
  *      }
