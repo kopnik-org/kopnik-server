3ea84c5a6ea23e6b91eb1d81361f36b325744706
Artem Ryzhkov <digital_boy@mail.ru>
2020-01-31 21:01:23 +0700

    coord indexes

---
+++ src/Migrations/Version20200131140019.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200131140019 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE INDEX IDX_1483A5E94118D123 ON users (latitude)');
+        $this->addSql('CREATE INDEX IDX_1483A5E985E16F6B ON users (longitude)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX IDX_1483A5E94118D123');
+        $this->addSql('DROP INDEX IDX_1483A5E985E16F6B');
+    }
+}
