71d681021c652508c020f9c7672b1dcf817adb0f
artem <artem.digi@gmail.com>
2020-03-11 12:25:38 +0700

    passport code as string

---
+++ src/Migrations/Version20200311052503.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200311052503 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE users ALTER passport_code TYPE VARCHAR(4)');
+        $this->addSql('ALTER TABLE users ALTER passport_code DROP DEFAULT');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE users ALTER passport_code TYPE INT');
+        $this->addSql('ALTER TABLE users ALTER passport_code DROP DEFAULT');
+        $this->addSql('ALTER TABLE users ALTER passport_code TYPE INT');
+    }
+}
