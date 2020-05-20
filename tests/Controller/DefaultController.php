8bedd0ef2c8990ad3b3fc861cbf6973da432a96f
artem <digital_boy@mail.ru>
2020-05-20 23:38:23 +0700

    CS fix

--- tests/Controller/DefaultController.php
+++ tests/Controller/DefaultController.php
@@ -13,7 +13,6 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,7 +21,7 @@ class DefaultController extends AbstractApiController
     /**
      * @Route("/api/test/setupDB", name="test_setup_db")
      */
-    public function setupDB(KernelInterface $kernel)
+    public function setupDB(KernelInterface $kernel): JsonResponse
     {
         $application = new Application($kernel);
         $application->setAutoExit(false);
