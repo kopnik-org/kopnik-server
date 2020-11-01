d6b390290f28bea0f410b1fa9d1d6611455b2b2d
artem <artem.digi@gmail.com>
2020-11-01 08:08:50 +0700

    deploy script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-SCRIPT_START_TIME=$(date +%s)
+SCRIPT_START_SECONDS=$(date +%s)
+SCRIPT_START_DATE=$(date +%T)
 
 make deploy
-date
 
-echo "Total time elapsed: $(date -ud "@$(($(date +%s) - $SCRIPT_START_TIME))" +%T)"
+SCRIPT_END_DATE=$(date +%T)
+
+echo "Deploy started  at: ${SCRIPT_START_DATE}"
+echo "Deploy finished at: ${SCRIPT_END_DATE}"
+echo "Total time elapsed: $(date -ud "@$(($(date +%s) - $SCRIPT_START_SECONDS))" +%T)"
